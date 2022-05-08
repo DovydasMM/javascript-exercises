4a96dcbaf7c3e59401fb3cfc09a4f8ac344255de
DovydasMM <dovistk@gmail.com>
2022-05-08 22:49:49 +0300

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,13 +3,13 @@ const removeFromArray = function(array,...removeElem) {
     for (let j = 0; j<removeElem.length;j++){
         let i = 0;
      while (true){ 
-        i++;
-        if (array[i]==removeElem[j]){
-        array.splice(i,1)
+        if (array[i]===removeElem[j]){
+        array.splice(i,1);
          break;
         } else if (i > array.length)
         break;{
         }
+        i++;
         };
    }
 console.log(array);
