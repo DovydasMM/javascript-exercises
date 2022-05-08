a7101fa3092e313dc17c11af8163157678236760
DovydasMM <dovistk@gmail.com>
2022-05-08 22:39:38 +0300

    Done till 4th

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...removeElem) {
 
-};
-
-// Do not edit below this line
+    for (let j = 0; j<removeElem.length;j++){
+        let i = 0;
+     while (true){ 
+        i++;
+        if (array[i]==removeElem[j]){
+        array.splice(i,1)
+         break;
+        } else if (i > array.length)
+        break;{
+        }
+        };
+   }
+console.log(array);
+return array;
+}
 module.exports = removeFromArray;
